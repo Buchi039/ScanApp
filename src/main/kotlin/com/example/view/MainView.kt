efd3db3250800b6f4d8dce0d03e0d22346b0b2c1
Michael Bucher <michael.bucher@toowoxx.de>
2020-12-09 13:01:59 +0100

    test commit 123

--- src/main/kotlin/com/example/view/MainView.kt
+++ src/main/kotlin/com/example/view/MainView.kt
@@ -3,7 +3,7 @@ package com.example.view
 import com.example.Styles
 import tornadofx.*
 
-class MainView : View("Hello TornadoFX TEST") {
+class MainView : View("Hello TornadoFX 123") {
     override val root = hbox {
         label(title) {
             addClass(Styles.heading)
