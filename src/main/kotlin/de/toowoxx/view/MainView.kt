0a8cf7ebc0684f462735fd640dd6b054265dfdd3
Michael Bucher <michael.bucher@toowoxx.de>
2021-01-20 09:08:44 +0100

    Code kommentiert

--- src/main/kotlin/de/toowoxx/view/MainView.kt
+++ src/main/kotlin/de/toowoxx/view/MainView.kt
@@ -10,15 +10,23 @@ import tornadofx.*
 
 class MainView : View() {
 
+
     val controller: MainController by inject()
     val userController: UserController by inject()
     val adminView: AdminView by inject()
-    var userbuttons = GridPane()
-    var menubar = menubar()
+
+    var userbuttons = GridPane()  // Gridpane mit allen Userbuttons
+    var menubar = menubar()       // Menubar am oberen Rand des Fensters
     override var root: Parent = vbox() {}
 
-
+    /**
+     *  Baut Oberfäche der MainView
+     */
     init {
+        //UserController initiieren
+        userController.init()
+
+        //Menubar -> Leiste am oberen Rand des Fensters
         menubar = menubar {
             menu("Bearbeiten") {
                 item("Admin").action {
@@ -34,12 +42,15 @@ class MainView : View() {
                 }
             }
         }
-
-        userController.init()
+        // Gridpane mit den Userbuttons erstellen und der View hinzufügen
         userbuttons = genUserButtonsGridpane(userController.getUsernames())
         root.add(userbuttons)
     }
 
+    /**
+     * Aktualisiert die Übersicht mit allen Userbuttons
+     *
+     */
     fun refreshUserbuttons() {
         userbuttons.clear()
         userbuttons.add(genUserButtonsGridpane(userController.getUsernames()))
@@ -51,12 +62,19 @@ class MainView : View() {
         this.setWindowMinSize(userbuttons.width, userbuttons.height + menubar.height + 25.0)
     }
 
+    /**
+     * Erstellt aus Liste mit Usernamen eine Liste aus Buttons für jeden User
+     *
+     * @param userList
+     * @return
+     */
     fun generateUserButtons(userList: List<String>): List<Button> {
         val buttonList = mutableListOf<Button>()
         for ((buttonCount, user) in userList.withIndex()) {
             val button = button(user) {
                 minWidth = 100.0
                 minHeight = 50.0
+                // Button um die Übersicht (mit allen Scan Profilen) zu öffnen
                 action {
                     controller.showScanbuttonView(user)
                 }
@@ -64,23 +82,29 @@ class MainView : View() {
                 columnRowIndex(buttonCount + 1, 0)
                 marginTopBottom(5.0)
                 marginLeftRight(5.0)
-
-
                 fillHeight = true
             }
             buttonList.add(button)
         }
-
         return buttonList
     }
 
+    /**
+     * Erstellt ein TornadoFX Gridpane mit den Userbuttons aus einer Liste der Usernamen
+     *
+     * @param userList
+     * @return
+     */
     fun genUserButtonsGridpane(userList: List<String>): GridPane {
         val buttons = generateUserButtons(userList)
         val gridPane = GridPane()
 
-        val maxCols = 8
-        var rowIndex = 1
-        var colIndex = 1
+
+        val maxCols = 8     // Anzahl der maximalen Spalten
+        var rowIndex = 1    // Index der Reihen Position
+        var colIndex = 1    // Index der Spaltenposition
+
+        // Jeden Button dem generierten Gridpane hinzufügen
         for (button in buttons) {
             gridPane.add(button, colIndex, rowIndex)
             colIndex++
